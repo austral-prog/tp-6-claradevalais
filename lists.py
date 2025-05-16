4a29e77c0b0b4c2406c2699fbc051553dc08d205
claradevalais <cdevalais@mail.austral.edu.ar>
2025-05-15 23:13:24 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,33 @@
-# Replace the "ANSWER HERE" with your answer
-
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    nueva_lista = list_to_remove_elements[:]  
 
+    if len(nueva_lista) > 5:
+        del nueva_lista[5]  
+
+    if len(nueva_lista) > 4:
+        del nueva_lista[4]  
+
+    if len(nueva_lista) > 0:
+        del nueva_lista[0]  
+    return nueva_lista
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    return ["Pink"] + list_to_add_elements + ["Yellow"] # Remove this line and implement
 
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
+    return len(list_to_check)==0 # Remove this line and implement
 
 
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
+    if len(list_to_compare1) >= 3 and len(list_to_compare2) >= 3:
+        return list_to_compare1[2] == list_to_compare2[2]
+    else:
+        return False
 
 
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    list_of_lists_to_modify[0] = list_of_lists_to_modify[0][:2]      
+    list_of_lists_to_modify[1] = list_of_lists_to_modify[1][1:4]     
+    list_of_lists_to_modify[2] = list_of_lists_to_modify[2][-2:]     
+    return list_of_lists_to_modify
